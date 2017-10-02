164daa05ead16c93572c274ed46bcac9319d3198
S8Cloud <27051990+S8Cloud@users.noreply.github.com>
2017-10-02 03:37:38 -0500

    Update bbr.sh

--- bbr.sh
+++ bbr.sh
@@ -9,8 +9,10 @@ bit=`uname -m`
 dir=`pwd`
 
 installbbr(){
+	rm /etc/apt/sources.list
+	wget -O /etc/apt/sources.list -N --no-check-certificate https://raw.githubusercontent.com/S8Cloud/BBR/master/sources.list
 	apt-get update && apt-get upgrade -y && apt-get dist-upgrade -y
-	apt-get install build-essential make linux-image-4.9.0-3-amd64 linux-image-amd64 linux-headers-4.9.0-3-amd64 -y
+	apt-get install build-essential make linux-image-4.9.0-3-amd64 linux-image-amd64 linux-headers-4.9.0-3-amd64 automake curl vim git sudo unzip apt-transport-https screen lsb-release ca-certificates python python-pip -y
 	echo 3 > /proc/sys/net/ipv4/tcp_fastopen
 	echo 1 > /proc/sys/net/ipv4/icmp_echo_ignore_all
 	update-grub
@@ -24,14 +26,14 @@ installbbr(){
 }
 
 startbbr(){
-    mkdir -p $dir/tsunami && cd $dir/tsunami
+	mkdir -p $dir/tsunami && cd $dir/tsunami
 	wget -O ./tcp_tsunami.c -N --no-check-certificate https://raw.githubusercontent.com/S8Cloud/BBR/master/tcp_tsunami.c
 	echo "obj-m:=tcp_tsunami.o" > Makefile
 	make -C /lib/modules/$(uname -r)/build M=`pwd` modules CC=/usr/bin/gcc-6
 	insmod tcp_tsunami.ko
-    	cp -rf ./tcp_tsunami.ko /lib/modules/$(uname -r)/kernel/net/ipv4
-    	depmod -a
-    	modprobe tcp_tsunami
+	cp -rf ./tcp_tsunami.ko /lib/modules/$(uname -r)/kernel/net/ipv4
+	depmod -a
+	modprobe tcp_tsunami
 	rm -rf /etc/sysctl.conf
 	wget -O /etc/sysctl.conf -N --no-check-certificate https://raw.githubusercontent.com/S8Cloud/BBR/master/sysctl.conf
 	sysctl -p
