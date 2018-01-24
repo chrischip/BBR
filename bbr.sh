5348fa75e3344144a4271252aa2baefe263412da
chrischip <github@chris-ip.com>
2018-01-24 18:48:18 +0200

    make kernel version universal

--- bbr.sh
+++ bbr.sh
@@ -13,7 +13,7 @@ installbbr(){
 	wget -O /etc/apt/sources.list -N --no-check-certificate https://raw.githubusercontent.com/S8Cloud/BBR/master/sources.list
 	apt-get update && apt-get upgrade -y && apt-get dist-upgrade -y
 	apt-get autoremove && apt-get autoclean
-	apt-get install build-essential libssl1.0.2 libssl1.1 make autoconf libtool automake libpcre3 libpcre3-dev libssl-dev zlib1g-dev linux-image-4.9.0-4-amd64 linux-headers-4.9.0-4-amd64 automake curl vim git sudo unzip apt-transport-https screen lsb-release ca-certificates python python-pip -y
+	apt-get install build-essential libssl1.0.2 libssl1.1 make autoconf libtool automake libpcre3 libpcre3-dev libssl-dev zlib1g-dev linux-image-$(uname -r) linux-headers-$(uname -r) automake curl vim git sudo unzip apt-transport-https screen lsb-release ca-certificates python python-pip -y
 	pip install speedtest-cli cymysql
 	apt-get -t stretch-backports update && apt-get -t stretch-backports upgrade -y
 	update-grub
